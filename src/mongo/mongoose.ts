ea3a53419c0a3c60dd6062fa5c81ed59f3285c42
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-07-14 22:58:12 +0800

    herkou deploy

--- src/mongo/mongoose.ts
+++ src/mongo/mongoose.ts
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 import { MONGO_URI } from '../utils/envsLoader';
 
 const connectDb = () => {
+    const uri = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : MONGO_URI;
     const db = mongoose.connection;
     console.log('MONGO_URI', MONGO_URI);
-    mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+    mongoose.connect(uri, { useNewUrlParser: true });
     db.on('error', console.error);
     db.on('connected', () => console.log('connected to mongo'));
     db.on('diconnected', () => console.log('Mongo is disconnected'));
